f08192d0148642d0a5dde3841f2290cbb9b1d638
jimmytidey <jimmytidey@gmail.com>
2012-02-22 22:18:22 +0000

    correcting bug with 0 bank options

--- html/list.php
+++ html/list.php
@@ -24,27 +24,29 @@ foreach ($bank_array as $bank) {
 	$bank_option_array = structure_list('projects/'.$project_name."/".$bank, "dir");
 	
 	$j = 0;
-	foreach ($bank_option_array as $bank_option) {
+	
+	if (count($bank_option_array)>0) {
+	
+		foreach ($bank_option_array as $bank_option) {
 		
-		$json['banks'][$i]['bank_name'] = $bank; 
+			$json['banks'][$i]['bank_name'] = $bank; 
+			$json['banks'][$i]['bank_options'][$j] = read_json('projects/'.$project_name."/".$bank."/".$bank_option."/bank_option_info.json");
+			$json['banks'][$i]['bank_options'][$j]['bank_option_name'] = $bank_option;
 		
+			//find the name of the wav file for this directory 
 		
-		$json['banks'][$i]['bank_options'][$j] = read_json('projects/'.$project_name."/".$bank."/".$bank_option."/bank_option_info.json");
-		$json['banks'][$i]['bank_options'][$j]['bank_option_name'] = $bank_option;
+			$audio_file_array = structure_list('projects/'.$project_name."/".$bank."/".$bank_option, "file");
 		
-		//find the name of the wav file for this directory 
-		
-		$audio_file_array = structure_list('projects/'.$project_name."/".$bank."/".$bank_option, "file");
-		
-		if (count($audio_file_array) > 0) {
-			foreach ($audio_file_array as $audio_file) {
+			if (count($audio_file_array) > 0) {
+				foreach ($audio_file_array as $audio_file) {
 						
-				if ($audio_file != 'bank_option_info.json') {
-					$json['banks'][$i]['bank_options'][$j]['file_location'] = 'projects/'.$project_name."/".$bank."/".$bank_option."/".$audio_file; 
+					if ($audio_file != 'bank_option_info.json') {
+						$json['banks'][$i]['bank_options'][$j]['file_location'] = 'projects/'.$project_name."/".$bank."/".$bank_option."/".$audio_file; 
+					}
 				}
-			}
+			}	
+			$j++;
 		}	
-		$j++;
 	}
 	$i++;
 }
