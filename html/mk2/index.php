0f31653ff0326545d8a3ba6f57623d04992777fc
jimmytidey <jimmytidey@gmail.com>
2012-04-13 23:00:37 +0100

    automated commit

--- html/mk2/index.php
+++ html/mk2/index.php
@@ -2,6 +2,10 @@
 include('header.php');
 include('functions.php'); 
 
+if (!is_dir('projects/'.$user_id) { 
+    mkdir('projects/'.$user_id.'/', 0777);    
+}
+
 if (!empty($_POST['project_name'])) 
 
 {
@@ -16,7 +20,7 @@ if (!empty($_POST['project_name']))
 		mkdir('projects/'.$user_id.'/'.$project_name, 0777);
 		
 		//copy template into the file 
-		$src = "list.jso";
+		$src = "list.json";
 		$dest = escapeshellarg('projects/'.$user_id.'/'.$project_name);
 		shell_exec("chmod -R -f 777 $dest"); 
 		shell_exec("cp -r $src $dest");
