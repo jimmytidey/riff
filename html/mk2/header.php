9f95678c21ed5471fa77e5f0fd9f1881c55ae326
jimmytidey <jimmytidey@gmail.com>
2012-04-13 22:28:21 +0100

    automated commit

---
+++ html/mk2/header.php
@@ -0,0 +1,32 @@
+<?php
+session_start(); 
+
+//make user validate
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Email jimmytidey@gmail.com if you forgot your password';
+    exit;
+} 
+
+if (isset($_SERVER['PHP_AUTH_USER'])) { 
+	//once they have validated, 
+	$authorisation = array('tom' => 'beard', 'ed' => 'williams', 'test' => 'test'); 
+
+	$allow = false;
+
+	foreach ($authorisation as $key => $value ) {
+		if ($_SERVER['PHP_AUTH_USER'] == $key && $_SERVER['PHP_AUTH_PW'] == $value) {
+			$allow = true; 
+			$user_id = $_SERVER['PHP_AUTH_USER'];
+			$_SESSION['user_id'] = $user_id; 
+		}
+	}
+
+	if ($allow != true) {
+	    header('WWW-Authenticate: Basic realm="My Realm"');
+	    header('HTTP/1.0 401 Unauthorized');
+		exit;
+	}
+}	
+?>
---
+++ html/mk2/index.php
@@ -0,0 +1,71 @@
+<?
+include('header.php');
+include('functions.php'); 
+
+if (!empty($_POST['project_name'])) 
+
+{
+	$project_name = $_POST['project_name']; 
+	
+	if (is_dir('projects/'.$user_id.'/'.$project_name)) {
+		echo "this project name already exists"; 
+	}
+	
+	else {
+		//make the project folder
+		mkdir('projects/'.$user_id.'/'.$project_name, 0777);
+		
+		//copy template into the file 
+		$src = "TEMPLATE/*";
+		$dest = escapeshellarg('projects/'.$user_id.'/'.$project_name);
+		shell_exec("chmod -R -f 777 $dest"); 
+		shell_exec("cp -r $src $dest");
+	}
+}
+ 
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+
+	<link rel='stylesheet' href="style/style.css" >
+</head>
+<body> 
+
+<div id='home_page'> 
+	
+	<h1 id='title'>MK 2 - RIFFF</h1>
+	
+	
+	<form method="post" action="index.php">
+		
+		<h3>Add new project</h3>
+		<p><input type='text' id='project_name' name='project_name' value='new project name' /><input type='submit' value='save new project' id='save_project_button' /></p>
+		 
+		
+	</form> 
+	
+	<h3>Existing projects</h3> 
+	<?
+	
+
+	// get each entry
+	$folder_array = structure_list('projects/'.$user_id.'/', 'dir');
+	
+	
+	foreach($folder_array as $project) {
+		
+		$encoded_name = urlencode($user_id.'/'.$project);
+		
+		echo "<a href='edit.php?project_name=$encoded_name'>$project</a><br/>";
+	
+	}
+	
+	
+	?>
+	
+</div> 
+
