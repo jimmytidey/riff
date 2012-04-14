6ae2c7ded6ce9d9d6e62ccc701dcdf91d4e5dc05
jimmytidey <jimmytidey@gmail.com>
2012-04-14 15:33:40 +0100

    automated commit

--- html/mk2/file_list.php
+++ html/mk2/file_list.php
@@ -1,8 +1,12 @@
 <?
-include('header.php'); 
+//include('header.php'); 
 include('functions.php');
 // get the project name from the get string 
 $project_name = $_GET['project_name'];
+
+if(!isset($user_id)) { 
+    $user_id = 'tom';
+}
 
 $list = structure_list("projects/$user_id/$project_name", 'file');
 $json = json_encode($list);
